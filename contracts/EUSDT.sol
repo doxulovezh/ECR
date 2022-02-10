af57d9d79de5d1daf234c25d85241c97bb734ce0
qinchuan <1841068286@qq.com>
2022-02-10 12:58:48 +0800

    1

--- contracts/EUSDT.sol
+++ contracts/EUSDT.sol
@@ -5,7 +5,26 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract EUSDT  is ERC20 {
-  constructor(uint256 initialSupply) ERC20("Pledge transaction token", "EUSDT") {
+  uint256 private aproveunlocked=1;
+  constructor(uint256 initialSupply) ERC20("Exchange USDT", "EUSDT") {
     _mint(msg.sender, initialSupply);
   }
+
+  function TranferBath(uint256[] calldata amounts,address[] calldata addrs) public approvelock returns  (bool) {
+    uint len=addrs.length;
+    uint len2=amounts.length;
+    require(len==len2,"amounts addrs not equal");
+    uint i=0;
+        for(i=0;i<len;i++){
+        transfer(addrs[i],amounts[i]);
+        }
+        return true;
+    }
+
+    modifier approvelock(){
+        require(aproveunlocked==1,"LOCKED");
+        aproveunlocked=0;
+        _;
+        aproveunlocked=1;
+    }
 }
